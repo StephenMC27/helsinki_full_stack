f045b8a56e5ed774c3be2494e68b383bccc59f3a
Stephen Clark <stephenmclark527@gmail.com>
2020-08-19 21:20:09 -0400

    Pull in data from RestCountries API and read input for filter - REMEMBER TO COMMIT MORE OFTEN

--- part2/data_for_countries/data-for-countries/src/App.js
+++ part2/data_for_countries/data-for-countries/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [ countries, setCountries ] = useState([])
+  const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('countries data received')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+      find countries: <input onChange={handleFilterChange} value={filter}/>
+    </form>
+  )
 }
 
 export default App;
